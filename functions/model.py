02bfcdcefe6fc16fb34813cd071a6f87957fd949
Chenbin Huang <24517165+ChenbinHuang@users.noreply.github.com>
2022-11-27 15:30:56 -0500

    transformer done, start translator

--- functions/model.py
+++ functions/model.py
@@ -1,6 +1,7 @@
+import sys
 import numpy as np
 import tensorflow as tf
-
+MAX_TOKENS=32
 def positional_encoding(length, depth):
   depth = depth/2
 
@@ -223,7 +224,9 @@ class Transformer(tf.keras.Model):
   def call(self, inputs):
     # To use a Keras model with `.fit` you must pass all your inputs in the
     # first argument.
+    # print(inputs)
     context, x  = inputs
+    # sys.stderr.write("DimContent={} / XShape{}\n".format(context.shape, x.shape))
 
     context = self.encoder(context)  # (batch_size, context_len, d_model)
 
@@ -242,3 +245,134 @@ class Transformer(tf.keras.Model):
     # Return the final output and the attention weights.
     return logits
 
+
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+  def __init__(self, d_model, warmup_steps=4000):
+    super().__init__()
+
+    self.d_model = d_model
+    self.d_model = tf.cast(self.d_model, tf.float32)
+
+    self.warmup_steps = warmup_steps
+
+  def __call__(self, step):
+    step = tf.cast(step, dtype=tf.float32)
+    arg1 = tf.math.rsqrt(step)
+    arg2 = step * (self.warmup_steps ** -1.5)
+
+    return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+
+def masked_loss(label, pred):
+  mask = label != 0
+  loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True, reduction='none')
+  loss = loss_object(label, pred)
+
+  mask = tf.cast(mask, dtype=loss.dtype)
+  loss *= mask
+
+  loss = tf.reduce_sum(loss)/tf.reduce_sum(mask)
+  return loss
+
+
+def masked_accuracy(label, pred):
+  pred = tf.argmax(pred, axis=2)
+  label = tf.cast(label, pred.dtype)
+  match = label == pred
+
+  mask = label != 0
+
+  match = match & mask
+
+  match = tf.cast(match, dtype=tf.float32)
+  mask = tf.cast(mask, dtype=tf.float32)
+  return tf.reduce_sum(match)/tf.reduce_sum(mask)
+
+class Translator(tf.Module):
+  def __init__(self, tokenizers, transformer):
+    self.tokenizers = tokenizers
+    self.transformer = transformer
+
+  def __call__(self, sentence, max_length=32):
+    # The input sentence is Portuguese, hence adding the `[START]` and `[END]` tokens.
+    assert isinstance(sentence, tf.Tensor)
+    if len(sentence.shape) == 0:
+      sentence = sentence[tf.newaxis]
+
+    sentence = self.tokenizers.pt.tokenize(sentence).to_tensor()
+
+    encoder_input = sentence
+
+    # As the output language is English, initialize the output with the
+    # English `[START]` token.
+    start_end = self.tokenizers.en.tokenize([''])[0]
+    start = start_end[0][tf.newaxis]
+    end = start_end[1][tf.newaxis]
+
+    # `tf.TensorArray` is required here (instead of a Python list), so that the
+    # dynamic-loop can be traced by `tf.function`.
+    output_array = tf.TensorArray(dtype=tf.int64, size=0, dynamic_size=True)
+    output_array = output_array.write(0, start)
+
+    for i in tf.range(max_length):
+      output = tf.transpose(output_array.stack())
+      predictions = self.transformer([encoder_input, output], training=False)
+
+      # Select the last token from the `seq_len` dimension.
+      predictions = predictions[:, -1:, :]  # Shape `(batch_size, 1, vocab_size)`.
+
+      predicted_id = tf.argmax(predictions, axis=-1)
+
+      # Concatenate the `predicted_id` to the output which is given to the
+      # decoder as its input.
+      output_array = output_array.write(i+1, predicted_id[0])
+
+      if predicted_id == end:
+        break
+
+    output = tf.transpose(output_array.stack())
+    # The output shape is `(1, tokens)`.
+    text = tokenizers.en.detokenize(output)[0]  # Shape: `()`.
+
+    tokens = tokenizers.en.lookup(output)[0]
+
+    # `tf.function` prevents us from using the attention_weights that were
+    # calculated on the last iteration of the loop.
+    # So, recalculate them outside the loop.
+    self.transformer([encoder_input, output[:,:-1]], training=False)
+    attention_weights = self.transformer.decoder.last_attn_scores
+
+    return text, tokens, attention_weights
+
+# def plot_attention_head(in_tokens, translated_tokens, attention):
+#   # The model didn't generate `<START>` in the output. Skip it.
+#   translated_tokens = translated_tokens[1:]
+
+#   ax = plt.gca()
+#   ax.matshow(attention)
+#   ax.set_xticks(range(len(in_tokens)))
+#   ax.set_yticks(range(len(translated_tokens)))
+
+#   labels = [label.decode('utf-8') for label in in_tokens.numpy()]
+#   ax.set_xticklabels(
+#       labels, rotation=90)
+
+#   labels = [label.decode('utf-8') for label in translated_tokens.numpy()]
+#   ax.set_yticklabels(labels)
+
+# def plot_attention_weights(sentence, translated_tokens, attention_heads):
+#   in_tokens = tf.convert_to_tensor([sentence])
+#   in_tokens = tokenizers.pt.tokenize(in_tokens).to_tensor()
+#   in_tokens = tokenizers.pt.lookup(in_tokens)[0]
+
+#   fig = plt.figure(figsize=(16, 8))
+
+#   for h, head in enumerate(attention_heads):
+#     ax = fig.add_subplot(2, 4, h+1)
+
+#     plot_attention_head(in_tokens, translated_tokens, head)
+
+#     ax.set_xlabel(f'Head {h+1}')
+
+#   plt.tight_layout()
+#   plt.show()
